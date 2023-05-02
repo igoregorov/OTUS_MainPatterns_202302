ee387e58d5417aa81ce7f21cb5a82ec555a638c4
Igor Egorov <iegorov@gmail.com>
2023-05-02 22:05:33 +0300

    HW01l02 Pattern Command

--- PatternCommand/src/main/java/ru/igoregorov/CheckFuelCommand.java
+++ PatternCommand/src/main/java/ru/igoregorov/CheckFuelCommand.java
@@ -9,10 +9,6 @@ public class CheckFuelCommand implements Command {
     }
     @Override
     public void execute() throws CommandException{
-        System.out.println(obj.getFuel());
-        System.out.println(obj.getDFuel());
-        System.out.println(isZero(obj.getFuel()) || isZero(obj.getFuel() - obj.getDFuel()) ||
-                (obj.getFuel() - obj.getDFuel()) < 0);
         if (isZero(obj.getFuel()) || isZero(obj.getFuel() - obj.getDFuel()) || (obj.getFuel() - obj.getDFuel()) < 0) {
             throw new CommandException(CommandException.Kind.NO_FUEL_FOR_MOVEMENT);
         }
